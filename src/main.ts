17cff75f7f6390ff7aa87dc1d263369ac3399158
Sumdy <sumdy@mail.ru>
2023-11-21 12:06:12 +0300

    rename entrypiont

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function root() {
   const app = await NestFactory.create(AppModule);
   await app.listen(8080);
 }
 
-bootstrap();
+root();
